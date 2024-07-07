c0f7f1d8521d56fc4f399af449c6c9c0fd153205
MAJDER0 <tomasz.majder89@gmail.com>
2024-07-07 21:53:03 +0200

    Stylization of Extension

--- popup.js
+++ popup.js
@@ -2,8 +2,9 @@ document.addEventListener('DOMContentLoaded', function () {
     const messageInput = document.getElementById('message');
     const changeButton = document.getElementById('change');
 
-    chrome.storage.local.get('savedMessage', (data) => {
+    chrome.storage.local.get(['savedMessage', 'isMessageCentered'], (data) => {
         messageInput.value = data.savedMessage || '';
+        messageInput.style.textAlign = data.isMessageCentered ? 'center' : 'left';
     });
 
     changeButton.addEventListener('click', function () {
@@ -14,10 +15,19 @@ document.addEventListener('DOMContentLoaded', function () {
 
         if (!isDisabled) {
             const savedMessage = messageInput.value;
-            chrome.storage.local.set({ 'savedMessage': savedMessage }, () => {
+            chrome.storage.local.set({
+                'savedMessage': savedMessage,
+                'isMessageCentered': true
+            }, () => {
                 console.log('Saved message to local storage:', savedMessage);
                 chrome.runtime.sendMessage({ action: 'updateSavedMessage', savedMessage: savedMessage });
+                // Center the text
+                messageInput.style.textAlign = 'center';
             });
+        } else {
+            // Reset text alignment when enabling the input
+            messageInput.style.textAlign = 'left';
+            chrome.storage.local.set({ 'isMessageCentered': false });
         }
     });
 });
